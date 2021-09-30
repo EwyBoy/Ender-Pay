eed55b72f2e60eb43694df22f7441c6b9ad661f4
Ewy <ewyboy@gmail.com>
2021-09-30 20:48:06 +0200

    Init()

---
+++ src/main/java/com/ewyboy/enderpay/EnderPay.java
@@ -0,0 +1,16 @@
+package com.ewyboy.enderpay;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.ewyboy.enderpay.EnderPay.MOD_ID;
+
+@Mod(MOD_ID)
+public class EnderPay {
+
+    public static final String MOD_ID = "enderpay";
+
+    public EnderPay() {
+
+    }
+
+}
